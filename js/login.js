b96f99b4eeef781e8b9e2a07dc62146ba560e354
frncsjnl <francisjaniel.alogon.iics@ust.edu.ph>
2021-09-28 13:37:03 +0800

    initial project

---
+++ js/login.js
@@ -0,0 +1,17 @@
+var btn = document.getElementById("modalBtn");
+var modal = document.getElementById("modalbg");
+var span = document.getElementsByClassName("closebtn")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
