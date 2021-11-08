499925f28838b7129f7110a41978da747b46ff44
frncsjnl <francisjaniel.alogon.iics@ust.edu.ph>
2021-11-08 22:05:45 +0800

    added faculty list, modified other pages

---
+++ js/table.js
@@ -0,0 +1,13 @@
+$(document).ready(function($)
+  {
+    //Delete row
+    $(document).on('click',".btn-row-delete", function(e)
+    {
+      var r = $(this).closest('tr').remove();
+    });
+
+    //View row
+
+    //Edit row
+    
+  });
